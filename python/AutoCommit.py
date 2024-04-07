08b8169486aed46a8e6b75d722a65601ac522115
Raulzada <rrnas27@gmail.com>
2024-04-07 02:39:25 -0300

    AutoCommit

--- python/AutoCommit.py
+++ python/AutoCommit.py
@@ -1,11 +1,14 @@
 import pyautogui as bot
+import time as tm
 
 bot.PAUSE = 1
 
 bot.hotkey('ctrl','shift','`')
 
-comandos = ["git add ." , "git commit -m 'AutoCommit' " , "git push"]
+comandos = ["git add ." , "git commit -m 'AutoCommit' " , "git push", "exit"]
 
 for c in comandos:
+    if c == "exit":
+        tm.sleep(5)
     bot.write(c)
     bot.press('Enter')
